ad262c258198c52eb131c50a9aa49d6f83123d57
Zach Buttram <zachb@hey.com>
2021-06-05 21:22:27 -0700

    move followed streams to top of sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -440,6 +440,67 @@ function Sidebar({
           </button>
         </label>
       </div>
+      {!hasTwitchAuth && (
+        <div>
+          <label>
+            <button
+              className="btn-sidebar bg-black bg-purple-700"
+              onClick={() => (window.location.href = TWITCH_AUTH_URL)}
+            >
+              <FontAwesomeIcon icon={faTwitch} fixedWidth />
+            </button>
+            <span className="btn-txt">Connect to Twitch</span>
+          </label>
+        </div>
+      )}
+      {followedStreams.length ? (
+        <>
+          <hr />
+          <div className="overflow-y-auto overflow-x-hidden py-2 bg-gray-900">
+            {followedStreams.map(({ stream, user }) => (
+              <div
+                className={
+                  streams
+                    .map((s) => s.toLowerCase())
+                    .includes(stream.userName.toLowerCase())
+                    ? "bg-blue-800 bg-opacity-50"
+                    : ""
+                }
+              >
+                <label className="flex">
+                  <button
+                    className="btn-sidebar-followed w-8 flex-shrink-0"
+                    onClick={() => addStream(stream.userName)}
+                  >
+                    <img
+                      className={classNames("rounded-full")}
+                      src={user!.profileImageUrl}
+                      alt={stream.userName}
+                    />
+                  </button>
+                  <div className="btn-txt flex-grow flex flex-col">
+                    <div className="flex justify-between">
+                      <div
+                        className="sidebar-stream-name"
+                        title={stream.userName}
+                      >
+                        {stream.userName}
+                      </div>
+                      <div className="text-xs ml-auto mr-2 text-red-400">
+                        {stream.viewerCount}
+                      </div>
+                    </div>
+                    <div className="sidebar-stream-title" title={stream.title}>
+                      {stream.title}
+                    </div>
+                  </div>
+                </label>
+              </div>
+            ))}
+          </div>
+          <hr className="mb-2" />
+        </>
+      ) : null}
       <div>
         <label>
           <button
@@ -502,76 +563,18 @@ function Sidebar({
           </span>
         </label>
       </div>
-      {!hasTwitchAuth && (
-        <div>
-          <label>
-            <button
-              className="btn-sidebar bg-black bg-purple-700"
-              onClick={() => (window.location.href = TWITCH_AUTH_URL)}
-            >
-              <FontAwesomeIcon icon={faTwitch} fixedWidth />
-            </button>
-            <span className="btn-txt">Connect to Twitch</span>
-          </label>
-        </div>
-      )}
-      {followedStreams.length ? (
-        <>
-          <hr className="mt-2" />
-          <div className="overflow-y-auto overflow-x-hidden py-2 bg-gray-900">
-            {followedStreams.map(({ stream, user }) => (
-              <div
-                className={
-                  streams
-                    .map((s) => s.toLowerCase())
-                    .includes(stream.userName.toLowerCase())
-                    ? "bg-blue-800 bg-opacity-50"
-                    : ""
-                }
-              >
-                <label className="flex">
-                  <button
-                    className="btn-sidebar-followed w-8 flex-shrink-0"
-                    onClick={() => addStream(stream.userName)}
-                  >
-                    <img
-                      className={classNames("rounded-full")}
-                      src={user!.profileImageUrl}
-                      alt={stream.userName}
-                    />
-                  </button>
-                  <div className="btn-txt flex-grow flex flex-col">
-                    <div className="flex justify-between">
-                      <div
-                        className="sidebar-stream-name"
-                        title={stream.userName}
-                      >
-                        {stream.userName}
-                      </div>
-                      <div className="text-xs ml-auto mr-2 text-red-400">
-                        {stream.viewerCount}
-                      </div>
-                    </div>
-                    <div className="sidebar-stream-title" title={stream.title}>
-                      {stream.title}
-                    </div>
-                  </div>
-                </label>
-              </div>
-            ))}
-          </div>
-          <hr />
-        </>
-      ) : null}
       <div className="flex-grow" />
-      <div className="my-3">
+      <div className="mb-3 mt-2">
         <label>
-          <a href={PROJECT_URL} target="_blank" rel="noreferrer">
-            <span className="btn-sidebar bg-black">
-              <FontAwesomeIcon icon={faGithub} fixedWidth />
-            </span>
-            <span className="btn-txt">GitHub</span>
+          <a
+            href={PROJECT_URL}
+            target="_blank"
+            className="btn-sidebar bg-black"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} fixedWidth />
           </a>
+          <span className="btn-txt">GitHub</span>
         </label>
       </div>
     </div>
